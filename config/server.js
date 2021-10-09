9f405c80b4afd1991cd78c01a2d30c9afed40982
adzanmend <adzanmendrofa3@gmail.com>
2021-10-09 13:59:33 +0700

    projek

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5cb6e39ae2b57923a908dbf96b95fa08'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd46cf606-912d-43f5-b857-9b0d829c5101'
+};
